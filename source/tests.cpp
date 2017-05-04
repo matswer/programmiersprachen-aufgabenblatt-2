f835b63f09b433a320f04bad7a41d4d40c6e8473
mats werchohlad <mats.werchohlad@uni-weimar.de>
2017-05-04 16:32:43 +0200

    Add tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,7 +3,7 @@
 #include "vec2.hpp"
 
 
-
+//Aufgabe 2.3
 TEST_CASE ("describe_vec2.hpp", "[vec2.hpp]")
 { 
 Vec2 leer{};
@@ -14,6 +14,7 @@ REQUIRE(Approx(full.x_) == 5.8664);
 REQUIRE(Approx(full.y_) == 8.002);
 }
 
+//Aufgabe 2.3
 TEST_CASE ("Additions Operator", "[vec2.hpp]")
 { 
 Vec2 vector1 (2.0, 4.0);
@@ -59,8 +60,30 @@ full1 /= multifloat2;
 REQUIRE(Approx(full1.x_) == 1.0);
 REQUIRE(Approx(full1.y_) == 1.0);
 }
+//Aufgabe 2.4
+
+TEST_CASE ("Addition", "[vec2.hpp]")
+{ 
+Vec2 vector_v (2.0, 4.0);
+Vec2 vector_u(5.8, 8.0);
+Vec2 vector_e(0.0, 0.0);
+vector_e = vector_v + vector_u;
+REQUIRE(Approx(vector_e.x_) == 7.8);
+REQUIRE(Approx(vector_e.y_) == 12.0);
+}
+
+TEST_CASE ("Subtraktion", "[vec2.hpp]")
+{ 
+Vec2 vector_v (2.0, 4.0);
+Vec2 vector_u(5.8, 8.0);
+Vec2 vector_e(0.0, 0.0);
+vector_e = vector_v - vector_u;
+REQUIRE(Approx(vector_e.x_) == -3.8);
+REQUIRE(Approx(vector_e.y_) == -4.0);
+}
 
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
 }
+
