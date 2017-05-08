ef49cf7903cb6df20bddc926bd3244e281b94b9b
mats werchohlad <mats.werchohlad@uni-weimar.de>
2017-05-08 16:12:56 +0200

    2.6 * Definition

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -26,12 +26,20 @@ Mat2 & Mat2::operator *= (Mat2 const & m)
 
 Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 )
 {
-Mat2 m3;
-m3.a_ = m1.a_ * m2.a_ + m1.b_ * m2.c_;
-m3.b_ = m1.a_ * m2.b_ + m1.b_ * m2.d_;
-m3.c_ = m1.c_ * m2.a_ + m1.d_ * m2.c_;
-m3.d_ = m1.c_ * m2.b_ + m1.d_ * m2.d_;
+    Mat2 m3;
+    m3.a_ = m1.a_ * m2.a_ + m1.b_ * m2.c_;
+    m3.b_ = m1.a_ * m2.b_ + m1.b_ * m2.d_;
+    m3.c_ = m1.c_ * m2.a_ + m1.d_ * m2.c_;
+    m3.d_ = m1.c_ * m2.b_ + m1.d_ * m2.d_;
 return m3;
 }
 
+Vec2 operator *( Mat2 const & m, Vec2 const & v)
+{
+    Vec2 v1;
+    v1.x_ = m.a_ * v.x_ + m.b_ * v.y_;
+    v1.y_ = m.b_ * v.y_;
+    return v1;
+
+}
 
