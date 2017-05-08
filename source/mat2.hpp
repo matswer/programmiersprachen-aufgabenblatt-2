7a160244b0c3c542d3a401e0532024e5874c2ff3
mats werchohlad <mats.werchohlad@uni-weimar.de>
2017-05-08 15:51:47 +0200

    2.6 Erste Änderung

--- source/mat2.hpp
+++ source/mat2.hpp
@@ -1,5 +1,8 @@
 #ifndef MAT2_HPP
 #define MAT2_HPP
+
+#include "Vec2.hpp"
+
 // Mat2 class definition
 struct Mat2
  {
@@ -14,6 +17,9 @@ struct Mat2
   };
 
 Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 );
+
+Vec2 operator *( Mat2 const & m, Vec2 const & v);
+Vec2 operator *( Vec2 const & v, Mat2 const & m);
 
 Mat2 inverse ( Mat2 const & m);
 Mat2 transpose ( Mat2 const & m);
